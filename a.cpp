a6ec44e512d642511cf9df81c7681327e377c67c
conGaStuer <xuankhuong1402@gmail.com>
2023-11-13 13:35:32 +0700

    haonthien

--- a.cpp
+++ a.cpp
@@ -6,4 +6,15 @@ int main(){
 	cout << "\nNhap n = ";
 	cin >> n;
 	
+bool Check_SHT(int N)
+{
+    int S = 0;
+    for (int i = 1; i < N; i++)
+        if (N % i == 0)
+            S += i;  // S = S + i
+
+    if (S == N)
+        return true; // Là số hoàn thiện
+    return false;     // Không là số hoàn thiện
+}
 }
